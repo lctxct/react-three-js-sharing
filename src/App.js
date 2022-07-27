1914d145d7f54b9bf2d9000a58627ab932205930
lctxct <caitinglien@hotmail.com>
2022-07-27 13:11:59 +0800

    Fix to start with HelloWorld

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-// import HelloWorld from "./pages/HelloWorld";
+import HelloWorld from "./pages/HelloWorld";
 // import World from "./pages/World";
-import GoodbyeWorld from "./pages/GoodbyeWorld";
+// import GoodbyeWorld from "./pages/GoodbyeWorld";
 
 function App() {
   return (
     <div id="canvas-container" style={{ height: "100vh" }}>
-      {/* <HelloWorld /> */}
+      <HelloWorld />
       {/* <World /> */}
-      <GoodbyeWorld />
+      {/* <GoodbyeWorld /> */}
     </div>
   );
 }
